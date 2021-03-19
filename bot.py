2d3477598a5228fdf9c66a731260c05ac6c6f33e
violettochka <80119123+violettochka@users.noreply.github.com>
2021-03-19 18:33:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler, MessageHandler, Filters
 import urllib.request
-link = 'https://ulkabo.github.io/bot-friday/data/'
+link = ' https://violettochka.github.io/bot_telega/data/'
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
